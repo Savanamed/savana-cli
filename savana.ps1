acc6230c867a66fd5fe443773c0d69787df0f0b9
iperez@savanamed.com <iperez@savanamed.com>
2021-12-02 15:55:24 +0100

    jue 02 dic 2021 15:55:24 CET

--- savana.ps1
+++ savana.ps1
@@ -20,15 +20,15 @@ switch ($args[0]) {
         $ret = [Regex]::Matches($js, "(?<=user/).+")
         $q = $ret[0].Value
         $r = $q.Substring(0,$q.Length-3)
-        aws s3 ls --recursive s3://timp-$r
+        aws s3 ls --recursive s3://$r
 }
 "sync" {
         $js = aws sts get-caller-identity
         $ret = [Regex]::Matches($js, "(?<=user/).+")
         $q = $ret[0].Value
         $r = $q.Substring(0,$q.Length-3)
-        aws s3 sync data s3://timp-$r
-        aws s3 sync s3://timp-$r/download data/download
+        aws s3 sync data s3://$r
+        aws s3 sync s3://$r/download data/download
 }
 default{
         Write-Host "incorrect option: savana help"
